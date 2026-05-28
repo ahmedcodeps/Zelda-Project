2fd4bdd31e1424d546976d417b0e074b44efd210
ahmedcodeps <ai7138572@gmail.com>
2026-05-28 21:44:45 +0300

    Delete project copy directory

--- project copy/conf.lua
+++
@@ -1,6 +0,0 @@
-
-
-function love.conf(t)
-    t.window.title = "The Legend Of Link"
-    t.window.icon = "images/Zelda.png"
-end
--- project copy/tiles.lua
+++
@@ -1,9 +0,0 @@
-
-local Tile = Entity:extend()
-
-
-function Tile:new(x,y,image)
-    Tile.super.new(self,x,y,image)
-end
-
-return Tile
